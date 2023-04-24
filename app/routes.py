2ece1b1445edff1c86c5c9058c4fe68acbc828fd
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-24 09:11:27 -0700

    Handle books not found and invalid ids

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -14,24 +14,42 @@ books = [
     
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description,
-        })
-    return jsonify(books_response), 200
-
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
+        books_response.append(
+            {
                 "id": book.id,
                 "title": book.title,
                 "description": book.description
             }
+        )
+    return jsonify(books_response)
+
+ 
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+
