0a13893d0a7027996a202b3a7dab086794520956
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-27 23:49:43 -0700

    Finished create model record

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
-from os import abort
-from flask import Blueprint, jsonify, abort, make_response
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
+
+
+
+
+
+
+
 
 # class Book:
 #     def __init__(self, id, title, description):
@@ -21,7 +42,7 @@ from flask import Blueprint, jsonify, abort, make_response
 #     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
 # ]
     
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+# books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
 # def validate_book(book_id):
