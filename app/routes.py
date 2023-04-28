07873b718acafe2f1e5bc2918001031201d8f064
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-28 00:26:55 -0700

    Finished read all books endpoint

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import db
 from app.models.book import Book
 from flask import Blueprint, jsonify, make_response, request
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
@@ -15,6 +15,19 @@ def handle_books():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
 
 
 
