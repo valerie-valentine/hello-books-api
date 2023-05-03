e72e0a6730faeb18df26699415520272ea004a8a
Valerie Valentin <valerie.valentine93@gmail.com>
2023-05-03 09:59:34 -0700

    Update inite with test config & test setup

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,5 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
+from os import abort
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
 
 @books_bp.route("", methods=["POST"])
 def create_book():
@@ -29,10 +43,36 @@ def read_all_books():
         )
     return jsonify(books_response)
 
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
 
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
 
+    request_body = request.get_json()
 
+    book.title = request_body["title"]
+    book.description = request_body["description"]
 
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted", 200)
 
 
 
